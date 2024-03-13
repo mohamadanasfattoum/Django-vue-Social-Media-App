571b88cfb543afede3da1590be4fbde26c986e43
Mohamad anas fattoum <samaanas30@gmail.com>
2024-03-13 15:31:02 +0100

    add axios.defaults.baseURL as django baseUrl

--- frontend/vue-frontend/src/main.js
+++ frontend/vue-frontend/src/main.js
@@ -7,9 +7,11 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router, axios)
 
 app.mount('#app')
