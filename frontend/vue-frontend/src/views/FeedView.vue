b15c36790a41e2361de5793f19406b535f7e1eaa
Mohamad anas fattoum <samaanas30@gmail.com>
2024-03-20 23:12:58 +0100

    create component Trends and add to pages

--- frontend/vue-frontend/src/views/FeedView.vue
+++ frontend/vue-frontend/src/views/FeedView.vue
@@ -115,57 +115,9 @@
         <div class="main-right col-span-1 space-y-4">
 
             <PeopleYouMayKnow/>
+            <Trends/>
 
-            <div class="p-4 bg-white border border-gray-200 rounded-lg">
-                <h3 class="mb-6 text-xl">Trends</h3>
 
-                <div class="space-y-4">
-                    <div class="flex items-center justify-between">
-                            <p class="text-xs">
-                                <strong>#lifegoal</strong><br>
-                                <span class="text-gray-500">180 posts</span>
-                            </p>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                            <p class="text-xs">
-                                <strong>#lifegoal</strong><br>
-                                <span class="text-gray-500">180 posts</span>
-                            </p>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                            <p class="text-xs">
-                                <strong>#lifegoal</strong><br>
-                                <span class="text-gray-500">180 posts</span>
-                            </p>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                            <p class="text-xs">
-                                <strong>#lifegoal</strong><br>
-                                <span class="text-gray-500">180 posts</span>
-                            </p>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                            <p class="text-xs">
-                                <strong>#lifegoal</strong><br>
-                                <span class="text-gray-500">180 posts</span>
-                            </p>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                    </div>
-                </div>
-            </div>
         </div>
 
     </div>    
@@ -174,12 +126,14 @@
 
 <script>
 import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
+import Trends from '../components/Trends.vue'
 
 export default{
     name: 'FeedView',
 
     components: {
         PeopleYouMayKnow,
+        Trends,
     }
 }
 
--- frontend/vue-frontend/src/views/SearchView.vue
+++ frontend/vue-frontend/src/views/SearchView.vue
@@ -101,56 +101,7 @@
         
         <PeopleYouMayKnow/>
 
-        <div class="p-4 bg-white border border-gray-200 rounded-lg">
-            <h3 class="mb-6 text-xl">Trends</h3>
-
-            <div class="space-y-4">
-                <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#lifegoal</strong><br>
-                            <span class="text-gray-500">180 posts</span>
-                        </p>
-
-                    <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                </div>
-
-                <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#lifegoal</strong><br>
-                            <span class="text-gray-500">180 posts</span>
-                        </p>
-
-                    <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                </div>
-
-                <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#lifegoal</strong><br>
-                            <span class="text-gray-500">180 posts</span>
-                        </p>
-
-                    <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                </div>
-
-                <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#lifegoal</strong><br>
-                            <span class="text-gray-500">180 posts</span>
-                        </p>
-
-                    <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                </div>
-
-                <div class="flex items-center justify-between">
-                        <p class="text-xs">
-                            <strong>#lifegoal</strong><br>
-                            <span class="text-gray-500">180 posts</span>
-                        </p>
-
-                    <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Explore</a>
-                </div>
-            </div>
-        </div>
+        <Trends/>
     </div>
 </div>
 </template>
@@ -158,12 +109,16 @@
 
 <script>
 import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
+import Trends from '../components/Trends.vue'
+
 
 export default{
     name: 'SearchView',
 
     components: {
         PeopleYouMayKnow,
+        Trends,
+
     }
 }
 
