53bbeea860e64608840dabc6615505dd088049c6
Mohamad anas fattoum <samaanas30@gmail.com>
2024-03-20 23:01:11 +0100

    create component PeopleYouMayKnow

---
+++ frontend/vue-frontend/src/components/PeopleYouMayKnow.vue
@@ -0,0 +1,39 @@
+<template>
+    
+    <div class="p-4 bg-white border border-gray-200 rounded-lg">
+        <h3 class="mb-6 text-xl">People you may know</h3>
+
+        <div class="space-y-4">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-3">
+                    <img src="https://mohamadanasfattoum.github.io/tailwindcss-2024/dist/images/adam.jpg" class="w-[40px] rounded-full">
+                    
+                    <p class="text-xs"><strong>Adam</strong></p>
+                </div>
+
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-3">
+                    <img src="https://mohamadanasfattoum.github.io/tailwindcss-2024/dist/images/leao.jpg" class="w-[40px] rounded-full">
+                    
+                    <p class="text-xs"><strong>Leao</strong></p>
+                </div>
+
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
+            </div>
+
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-3">
+                    <img src="https://mohamadanasfattoum.github.io/tailwindcss-2024/dist/images/masi.jpg" class="w-[40px] rounded-full">
+                    
+                    <p class="text-xs"><strong>Masi</strong></p>
+                </div>
+
+                <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
+            </div>
+        </div>
+    </div>
+
+</template>
