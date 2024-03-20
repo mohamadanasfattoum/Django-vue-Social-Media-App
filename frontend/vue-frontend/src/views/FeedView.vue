f267cede7c2aceee5f08acdef0790715fda06a0a
Mohamad anas fattoum <samaanas30@gmail.com>
2024-03-20 23:07:30 +0100

    add component PeopleYouMayKnow ti feed page

--- frontend/vue-frontend/src/views/FeedView.vue
+++ frontend/vue-frontend/src/views/FeedView.vue
@@ -113,41 +113,8 @@
 
 
         <div class="main-right col-span-1 space-y-4">
-            <div class="p-4 bg-white border border-gray-200 rounded-lg">
-                <h3 class="mb-6 text-xl">People you may know</h3>
 
-                <div class="space-y-4">
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-3">
-                            <img src="https://mohamadanasfattoum.github.io/tailwindcss-2024/dist/images/adam.jpg" class="w-[40px] rounded-full">
-                            
-                            <p class="text-xs"><strong>Adam</strong></p>
-                        </div>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-3">
-                            <img src="https://mohamadanasfattoum.github.io/tailwindcss-2024/dist/images/leao.jpg" class="w-[40px] rounded-full">
-                            
-                            <p class="text-xs"><strong>Leao</strong></p>
-                        </div>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-                    </div>
-
-                    <div class="flex items-center justify-between">
-                        <div class="flex items-center space-x-3">
-                            <img src="https://mohamadanasfattoum.github.io/tailwindcss-2024/dist/images/masi.jpg" class="w-[40px] rounded-full">
-                            
-                            <p class="text-xs"><strong>Masi</strong></p>
-                        </div>
-
-                        <a href="#" class="py-2 px-3 bg-purple-600 text-white text-xs rounded-lg">Show</a>
-                    </div>
-                </div>
-            </div>
+            <PeopleYouMayKnow/>
 
             <div class="p-4 bg-white border border-gray-200 rounded-lg">
                 <h3 class="mb-6 text-xl">Trends</h3>
@@ -203,3 +170,17 @@
 
     </div>    
 </template>
+
+
+<script>
+import PeopleYouMayKnow from '../components/PeopleYouMayKnow.vue'
+
+export default{
+    name: 'FeedView',
+
+    components: {
+        PeopleYouMayKnow,
+    }
+}
+
+</script>
