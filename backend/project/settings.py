f8b218f4f90f9906796bcc64fa942a7cceda25f3
Mohamad anas fattoum <samaanas30@gmail.com>
2024-03-21 02:07:43 +0100

    Configure for csrf and cors

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -53,15 +53,26 @@ REST_FRAMEWORK = {
 }
 
 
+CORS_ALLOWED_ORIGINS = [
+"http://127.0.0.1:5173", 
+]
+
+
+CSRF_TRUSTED_ORIGINS = [
+"http://127.0.0.1:5173", 
+]
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
+    'django.contrib.contenttypes', 
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'djangorestframework',
     'djangorestframework_simplejwt',
+    'corsheaders',
 
 
 ]
@@ -69,6 +80,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
