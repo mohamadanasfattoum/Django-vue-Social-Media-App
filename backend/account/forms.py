407189de84e8243de6a8a08c9ff8aa24c47c2bea
Mohamad anas fattoum <samaanas30@gmail.com>
2024-03-21 22:30:39 +0100

    Configuer SignupForm to in api.py

--- backend/account/forms.py
+++ backend/account/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 from .models import User
 
-class Signup(UserCreationForm):
+class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'name', 'password1', 'password2')
