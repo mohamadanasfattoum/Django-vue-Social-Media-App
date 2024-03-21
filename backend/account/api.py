26c731128fe25f6e7894d988adb74c41106f353e
Mohamad anas fattoum <samaanas30@gmail.com>
2024-03-21 22:08:49 +0100

    create forms.py and add class Signup

--- backend/account/api.py
+++ backend/account/api.py
@@ -1,4 +1,12 @@
 from django.http import JsonResponse
 
 from rest_framework.decorators import api_view , authentication_classes, permission_classes
+from .forms import Signup
 
+
+@api_view(['POST'])
+def signup(request):
+    data= request.data
+    message = 'success'
+
+    return JsonResponse({'status': message})
